7590c763438f1a2e06529d5b027c27f1895a6839
Thorsten Herber <thorsten.herber@ericsson.com>
2021-08-30 15:00:50 +0200

    Remove old control panel functions
    Change-Id: Ibdfd62542bab64c02d5de51e45afdf80c1fd97f6

--- main.cpp
+++ main.cpp
@@ -4,80 +4,6 @@
 #include "Memory.h"
 #include <stdio.h>
 
-void
-printCpuStatus(const Cpu& theCpu)
-{
-   printf("Registers:\n");
-   for (size_t i = 0; i < Cpu::NumRegsC; i++)
-   {
-      printf("              R%zu: %04X", i, theCpu.getRegister(i));
-      if (i == Cpu::PcRegC)
-      {
-         printf(" PC");
-      }
-      else
-      if ( i == Cpu::SpRegC)
-      {
-         printf(" SP");
-      }
-      printf("\n");
-   }
-   printf("\n");
-   printf("Address Register: %04X\n", theCpu.getAddressRegister());
-   printf("Instruction:      %04X\n", theCpu.getInstruction());
-   Cpu::Flags flags = theCpu.getFlags();
-   printf("Flags:            CF=%d, ZF=%d, NF=%d, OF=%d, IF=%d\n", 
-          flags.carry,
-          flags.zero,
-          flags.negative,
-          flags.overflow,
-          flags.enableInterupt);
-   printf("State:            ");
-   switch (theCpu.getState())
-   {
-      case Cpu::State::FetchInstrE:
-      {
-         printf("FetchInstr");
-         break;
-      }
-      case Cpu::State::DecodeInstrE: 
-      {
-         printf("DecodeInstr");
-         break;
-      }
-      case Cpu::State::WaitReadMemE:
-      {
-         printf("WaitReadMem");
-         break;
-      }
-      case Cpu::State::WaitWriteMemE:
-      {
-         printf("WaitWriteMem");
-         break;
-      }
-   }
-   printf("\n");
-}
-
-// ----------------------------------------------------------------------------
-
-void
-printMemory(
-   const Memory& theMemory,
-   uint16_t      theStartAddress,
-   uint16_t      theSize)
-{
-   const uint16_t* memory = theMemory.readData(theStartAddress, theSize);
-   if (memory != nullptr)
-   {
-      for (int i = 0; i < theSize; i++)
-      {
-         printf("%04X: %04X\n", theStartAddress + i, memory[i]);
-      }
-   }
-}
-
-// ----------------------------------------------------------------------------
 
 int
 main(
@@ -112,22 +38,17 @@ main(
    memory.writeHex(0x0008, 0x1234);
    memory.writeHex(0x0009, 0x0001);
 
-   printMemory(memory, 0x0000, 9);
+         controlPanel.draw();
 
    while (true)
    {
-      printf("clockTick (c), memory (m), or quit (q)? ");
+      printf("clockTick (c), or quit (q)? ");
       int c = getchar();
       printf("\n");
       if (c == 'c')
       {
          cpu.clockTick();
          controlPanel.draw();
-      }
-      else
-      if (c == 'm')
-      {
-         printMemory(memory, 0x0000, 16);
       }
       else
       if (c == 'q')
